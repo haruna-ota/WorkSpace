a0d4801c5fb97ab865521ed0d7bcc3b1393175c6
haruna-ota <h-ota@biglobe.co.jp>
2018-05-31 11:12:08 +0900

    フォーマットの指摘をなおしました

--- src/fizzbuzz.java
+++ src/fizzbuzz.java
@@ -1,18 +1,15 @@
 public class fizzbuzz {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //"1〜100"までの数字でFizzBuzz
         int i;
-        for(i=1;i<=100;i++){
-            if((i%3 == 0) && (i%5 == 0)){   //15の倍数の時”Fizz Buzz”表示
+        for (i = 1; i <= 100; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0)) {   //15の倍数の時”Fizz Buzz”表示
                 System.out.println("Fizz Buzz");
-            }
-            else if(i%3 == 0){
+            } else if (i % 3 == 0) {
                 System.out.println("Fizz");         //3の倍数の時”Fizz”表示
-            }
-            else if(i%5 == 0){
+            } else if (i % 5 == 0) {
                 System.out.println("Buzz");         //5の倍数の時”Buzz”表示
-            }
-            else{
+            } else {
                 System.out.println(i);
             }
         }
