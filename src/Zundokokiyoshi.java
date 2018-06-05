10306a325f148430e8af381def3d55e7613d486c
haruna-ota <h-ota@biglobe.co.jp>
2018-06-05 13:37:47 +0900

    問題文通りに訂正しました

--- src/Zundokokiyoshi.java
+++ src/Zundokokiyoshi.java
@@ -4,16 +4,12 @@ public class Zundokokiyoshi {
         int rand;
         int flag = 0; //判定用フラグ
         String end = "ズン ズン ズン ズン ドコ "; //正解の文字列
-        String out = "ズン ズン ズン ズン ズン "; //ズンが５回続いたらカウントリセット
         StringBuilder sb = new StringBuilder();
 
         while (flag == 0) {
             rand = (int) (Math.random() * 10);     //1~10までをランダムに生成
             if (rand <= 4) {
                 sb.append(zundoko[0]);  //文字列を追加
-                if (sb.toString().endsWith(out)) {
-                    sb.setLength(0);
-                }
             } else {
                 sb.append(zundoko[1]);  //文字列を追加
                 if (sb.toString().endsWith(end)) {
