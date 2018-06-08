3c5bdcc9d924c4fc05a0202ebad4177d5ad5d609
haruna-ota <h-ota@biglobe.co.jp>
2018-06-08 16:37:29 +0900

    ファイル名変更&出力方法訂正しました

--- src/StrBack.java
+++ src/StrBack.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Str_back {
+public class StrBack {
     public static void main(String[] args) {
         int i;
         System.out.println("文字を入力");
@@ -8,9 +8,13 @@ public class Str_back {
         String word = scan.next();
         System.out.println("入力された文字：" + word);
         String[] word_list = word.split("");    //1文字ずつ切り分ける
-        System.out.print("出力される文字：");
+
+        StringBuilder sb = new StringBuilder();
+
         for (i = word.length() - 1; i >= 0; i--) {
-            System.out.print(word_list[i]);
+            sb.append(word_list[i]);    //後ろの文字から1つずつ追加
         }
+        String result = sb.toString();
+        System.out.println("出力される文字：" + result);
     }
 }
