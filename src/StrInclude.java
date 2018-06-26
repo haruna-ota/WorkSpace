69ffc2b9fe692de42d4885433de92238616db2cc
haruna-ota <h-ota@biglobe.co.jp>
2018-06-26 16:14:41 +0900

    ご指摘いただいた箇所の訂正をしました

--- src/StrInclude.java
+++ src/StrInclude.java
@@ -14,33 +14,31 @@ public class StrInclude {
     }
 
     private static boolean strIncludeCheck(String word, String word2) {
-        int i, j = 0;
-        int correctCount = 0;   //合致文字判定用
-
         //空文字判定
         if (word2.isEmpty()) {  //第二引数が空文字だったら絶対true
             return true;
         } else if (word.isEmpty()) { //第二引数が空文字じゃない状態で第一引数が空文字の場合false
             return false;
+        } else if (word.length() < word2.length()) {   //第一引数より第二引数が長い場合はfalse
+            return false;
         } else {    //それ以外は一文字ずつ判定開始
-            String[] wordList = word.split("");    //第一引数を1文字ずつ切り分ける
-            String[] word2List = word2.split("");    //第二引数を1文字ずつ切り分ける
+            int correctCount = 0;   //合致文字判定用
+            String[] wordArray = word.split("");    //第一引数を1文字ずつ切り分ける
+            String[] word2Array = word2.split("");    //第二引数を1文字ずつ切り分ける
 
-            if (wordList.length < word2List.length) {   //第一引数より第二引数が長い場合はfalse
-                return false;
-            } else {
-                //照合開始位置確認
-                for (i = 0; i <= wordList.length - 1; i++) {
-                    if (wordList[i].equals(word2List[j])) {
-                        correctCount++;
-                        j++;
-                    } else {    //連続して合致しなかった場合はカウントを戻す
-                        correctCount = 0;
-                        j = 0;
-                    }
+            //照合開始位置確認
+            int i, j = 0;
+
+            for (i = 0; i <= wordArray.length - 1; i++) {
+                if (wordArray[i].equals(word2Array[j])) {
+                    correctCount++;
+                    j++;
+                } else {    //連続して合致しなかった場合はカウントを戻す
+                    correctCount = 0;
+                    j = 0;
                 }
             }
-            return word2List.length == correctCount;    //照合した文字全てが合致したらtrue
+            return word2Array.length == correctCount;    //照合した文字全てが合致したらtrue
         }
     }
 }
