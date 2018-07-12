956d9b58a3516cc376645438edca90622fdc3f36
haruna-ota <h-ota@biglobe.co.jp>
2018-07-12 12:01:30 +0900

    ご指摘いただいた箇所を直しました

--- src/main/java/_10_language/Language.java
+++ src/main/java/_10_language/Language.java
@@ -1,46 +1,41 @@
 package _10_language;
 
 public class Language {
-    private LanguageEnum languages;
+    private final LanguageEnum language;
 
     //コンストラクタ
-    Language(LanguageEnum languages) {
-        this.languages = languages;
+    public Language(LanguageEnum language) {
+        this.language = language;
     }
 
 
     //挨拶を返すメソッド
     public String greeting(String name) {
-        switch (name) {
-            case "太郎":
-                return "こんにちは、" + name;
-            case "John":
-                return "hello " + name;
-            case "Marco":
-                return "buon giorno " + name;
+        if (language == (LanguageEnum.JAPAN)) {  //日本語の場合は日本語で返す(Enumは==で比較)
+            return "こんにちは、" + name;
+        } else if (language == (LanguageEnum.ENGLISH)) { //英語の場合は英語で返す
+            return "hello " + name;
+        } else {                                   //それ以外はイタリア語で返す
+            return "buon giorno " + name;
         }
-        return null;
     }
 
     //数えるときの単位を変えすメソッド
     public String nums(String animalName) {
-        switch (animalName) {
-            case "犬":
-                return "匹";
-            case "うさぎ":
-                return "羽";
+        if (animalName.equals("犬")) {
+            return "匹";
+        } else {
+            return "羽";
         }
-        return null;
     }
 
     //男性名詞か女性名詞かを教えてくれるメソッド
     public String noun(String noun) {
-        switch (noun) {
-            case "列車":
-                return "男性";
-            case "車":
-                return "女性";
+        assert language == (LanguageEnum.ITALIAN);   //イタリア語かどうかの確認
+        if (noun.equals("列車")) {
+            return "男性";
+        } else {
+            return "女性";
         }
-        return null;
     }
 }
--- src/test/java/_10_language/LanguageTest.java
+++ src/test/java/_10_language/LanguageTest.java
@@ -7,18 +7,20 @@ import static org.junit.Assert.*;
 public class LanguageTest {
     @Test
     public void Language() {
-        Language language1 = new Language(LanguageEnum.JAPAN);
-        assertEquals("こんにちは、太郎", language1.greeting("太郎"));
-        assertEquals("匹", language1.nums("犬"));
-        assertEquals("羽", language1.nums("うさぎ"));
+        Language japanese = new Language(LanguageEnum.JAPAN);
+        assertEquals("こんにちは、太郎", japanese.greeting("太郎"));
+        assertEquals("匹", japanese.nums("犬"));
+        assertEquals("羽", japanese.nums("うさぎ"));
 
-        Language language2 = new Language(LanguageEnum.ENGLISH);
-        assertEquals("hello John", language2.greeting("John"));
+        Language english = new Language(LanguageEnum.ENGLISH);
+        assertEquals("hello John", english.greeting("John"));
 
-        Language language3 = new Language(LanguageEnum.ITALIAN);
-        assertEquals("buon giorno Marco", language3.greeting("Marco"));
-        assertEquals("男性", language3.noun("列車"));
-        assertEquals("女性", language3.noun("車"));
+        Language italian = new Language(LanguageEnum.ITALIAN);
+        assertEquals("buon giorno Marco", italian.greeting("Marco"));
+        assertEquals("男性", italian.noun("列車"));
+        assertEquals("女性", italian.noun("車"));
 
+        assertEquals("hello 太郎", english.greeting("太郎"));
+        //assertEquals("男性", japanese.noun("列車"));
     }
 }
