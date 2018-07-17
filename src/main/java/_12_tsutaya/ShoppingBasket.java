70098dd672af6dc0eca7185cc362e2a9e890222c
haruna-ota <h-ota@biglobe.co.jp>
2018-07-17 18:05:27 +0900

    ご指摘いただいたところを修正しました

--- src/main/java/_12_tsutaya/ShoppingBasket.java
+++ src/main/java/_12_tsutaya/ShoppingBasket.java
@@ -11,7 +11,7 @@ public class ShoppingBasket {
     }
 
     //カゴの中身の合計金額を計算
-    public int CalculateToTotalAmount() {
+    public int calculateToTotalAmount() {
         int totalAmount = 0;
         for (RentalGoods rentalGoods : rentalGoodsList) {   //拡張for文（全ての要素に対して行う）
             totalAmount = totalAmount + rentalGoods.getPrice();    //i個目のpriceを取ってくる
@@ -20,11 +20,11 @@ public class ShoppingBasket {
     }
 
     //カゴの中身のタイトルを確認
-    public String ShowTitles() {
-        String titles = "";
-        for (RentalGoods rentalGoods : rentalGoodsList) {
-            titles = titles + rentalGoods.getTitle();
+    public String showTitles() {
+        String[] titles = new String[rentalGoodsList.size()];   //レンタルするもののタイトルだけを集める
+        for (int i = 0; i < rentalGoodsList.size(); i++) {
+            titles[i] = rentalGoodsList.get(i).getTitle();  //タイトルだけを配列に詰める
         }
-        return titles;
+        return String.join(", ", titles);   //,(スペース区切りにする)
     }
 }
--- src/test/java/_12_tsutaya/RentalGoodsTest.java
+++ src/test/java/_12_tsutaya/RentalGoodsTest.java
@@ -2,66 +2,58 @@ package _12_tsutaya;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class RentalGoodsTest {
+
+    RentalGoods cd = new Cd("タイトルA");
+    RentalGoods dvd = new Dvd("タイトルB");
+    RentalGoods comic = new Comic("タイトルC");
+
     //テスト1パターン目（CD,DVD,漫画を1つずつ借りる時）
     @Test
     public void RentalGoods1() {
-        RentalGoods cd = new Cd("タイトルA");
-        RentalGoods dvd = new Dvd("タイトルB");
-        RentalGoods comic = new Comic("タイトルC");
-
         //レンタルするものをリスト化（カゴに借りるものを入れる）
-        List<RentalGoods> rentalGoodsList = new ArrayList<>(Arrays.asList(cd, dvd, comic));
+        List<RentalGoods> rentalGoodsList = Arrays.asList(cd, dvd, comic);
         ShoppingBasket shoppingBasket = new ShoppingBasket(rentalGoodsList);
 
         //合計金額を計算する(assertSame)
-        assertEquals(600, shoppingBasket.CalculateToTotalAmount());
+        assertEquals(600, shoppingBasket.calculateToTotalAmount());
 
         //タイトルを表示する(assertEquals)
-        assertEquals("タイトルAタイトルBタイトルC", shoppingBasket.ShowTitles());
+        assertEquals("タイトルA, タイトルB, タイトルC", shoppingBasket.showTitles());
     }
 
     //テスト2パターン目（CDを1枚借りる時）
     @Test
     public void RentalGoods2() {
-        RentalGoods cd = new Cd("タイトルA");
-        RentalGoods dvd = new Dvd("タイトルB");
-        RentalGoods comic = new Comic("タイトルC");
-
         //レンタルするものをリスト化（カゴに借りるものを入れる）
-        List<RentalGoods> rentalGoodsList = new ArrayList<>(Arrays.asList(cd));
+        List<RentalGoods> rentalGoodsList = Arrays.asList(cd);
         ShoppingBasket shoppingBasket = new ShoppingBasket(rentalGoodsList);
 
         //合計金額を計算する(assertSame)
-        assertEquals(200, shoppingBasket.CalculateToTotalAmount());
+        assertEquals(200, shoppingBasket.calculateToTotalAmount());
 
         //タイトルを表示する(assertEquals)
-        assertEquals("タイトルA", shoppingBasket.ShowTitles());
+        assertEquals("タイトルA", shoppingBasket.showTitles());
 
     }
 
     //テスト3パターン目（なにも借りない時）
     @Test
     public void RentalGoods3() {
-        RentalGoods cd = new Cd("タイトルA");
-        RentalGoods dvd = new Dvd("タイトルB");
-        RentalGoods comic = new Comic("タイトルC");
-
         //レンタルするものをリスト化（カゴに借りるものを入れる）
-        List<RentalGoods> rentalGoodsList = new ArrayList<>(Arrays.asList());
+        List<RentalGoods> rentalGoodsList = Arrays.asList();
         ShoppingBasket shoppingBasket = new ShoppingBasket(rentalGoodsList);
 
         //合計金額を計算する(assertSame)
-        assertEquals(0, shoppingBasket.CalculateToTotalAmount());
+        assertEquals(0, shoppingBasket.calculateToTotalAmount());
 
         //タイトルを表示する(assertEquals)
-        assertEquals("", shoppingBasket.ShowTitles());
+        assertEquals("", shoppingBasket.showTitles());
     }
 
 }
